1f14e7dff3b3783b24cb503347fd2b367628d215
taoman <1344216101@qq.com>
2022-08-12 15:33:31 +0800

    更新路由

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,8 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     modules: ["@vueuse/nuxt"],
+    router:{
+        // base:process.env.NODE_ENV === 'production'?'/web/':'/'
+        base:'/web/'
+    }
 })
